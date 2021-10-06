26e541f8f0c65af05cfc0045a466138dfae83dab
julietafc <juli47g1@stud.kea.dk>
2021-10-06 21:21:10 +0200

    reverse effect but not working

--- script.js
+++ script.js
@@ -30,16 +30,6 @@ function toggleOption(event) {
   }
 
   // If feature is (now) turned on:
-  // - create featureElement and append to #selected ul
-  // - create FLIP-animation to animate featureElement from img in target, to
-  //   its intended position. Do it with normal animation or transition class!
-
-  // Else - if the feature (became) turned off:
-  // - find the existing featureElement in #selected ul
-  // - create FLIP-animation to animate featureElement to img in target
-  // - when animation is complete, remove featureElement from the DOM
-
-  // If feature is (now) turned on:
   if (features[feature]) {
     // feature added
     console.log(`Feature ${feature} is turned on!`);
@@ -52,9 +42,11 @@ function toggleOption(event) {
 
     // - create featureElement
     const featureElement = createFeatureElement(feature);
+
     // and append to #selected ul
     document.querySelector("#selected ul").appendChild(featureElement);
     featureElement.classList.add(`${feature}`);
+
     // - create FLIP-animation to animate featureElement from img in target, to
     //   its intended position. Do it with normal animation or transition class!
     const firstFrame = document.querySelector("#options").getBoundingClientRect();
@@ -89,19 +81,53 @@ function toggleOption(event) {
   } else {
     // feature removed
     console.log(`Feature ${feature} is turned off!`);
+
     // - no longer mark target as chosen
     target.classList.remove("chosen");
+
     // - hide the feature-layer(s) in the #product-preview
     document.querySelector(`[data-feature=${feature}]`).classList.add("hide");
+
     // - find the existing featureElement in #selected ul
+    let childElement = document.querySelector(`.${feature}`);
+
     // - create FLIP-animation to animate featureElement to img in target
+    const firstFrame = childElement.getBoundingClientRect();
+    const lastFrame = target.getBoundingClientRect();
+    console.log(firstFrame, lastFrame);
+
+    const deltaX = firstFrame.left - lastFrame.left;
+    const deltaY = firstFrame.top - lastFrame.top;
+    const deltaWith = firstFrame.width / lastFrame.width;
+    const deltaHeight = firstFrame.height / lastFrame.height;
+
     // - when animation is complete, remove featureElement from the DOM
+    document.querySelector("li").remove(childElement);
 
     // TODO: More code
+
+    childElement.animate(
+      [
+        {
+          transformOrigin: "bottom left",
+          transform: `translate(${deltaX}px, ${deltaY}px)
+          scale(${deltaWith}, ${deltaHeight})`,
+        },
+        {
+          transformOrigin: "bottom left",
+          transform: "none",
+        },
+      ],
+      {
+        duration: 500,
+        easing: "ease-in-out",
+      }
+    );
   }
 }
 
 // Create featureElement to be appended to #selected ul - could have used a <template> instead
+
 function createFeatureElement(feature) {
   const li = document.createElement("li");
   li.dataset.feature = feature;
